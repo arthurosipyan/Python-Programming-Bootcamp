b1533741bfe75d87046a86590a549413174238be
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2021-04-01 20:32:13 -0400

    Create CaesarCipher.py

---
+++ CaesarCipher.py
@@ -0,0 +1,6 @@
+# 1. Receive a message and then encrypt it by shifting the
+# characters by a requested amount to the right.
+
+# A becomes D, B becomes E, for example. Also
+# decrypt the message back.
+
