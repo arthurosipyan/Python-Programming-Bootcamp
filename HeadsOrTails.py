4c5f2a3732112aa18fe44cbb6ba10eb24c1cb9c2
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2021-04-18 15:22:08 -0400

    Create HeadsOrTails.py

---
+++ HeadsOrTails.py
@@ -0,0 +1,17 @@
+import random
+import time
+# Example Output
+# Heads: 46
+# Tails: 54
+
+
+def main():
+    start = time.time()
+    ht_list = [random.choice(list(['h', 't'])) for i in range(100)]
+    print("Heads: {} ({}%)".format(ht_list.count("h"), str(int(((ht_list.count("h")/len(ht_list)) * 100)))))
+    print("Tails: {} ({}%)".format(ht_list.count("t"), str(int(((ht_list.count("t")/len(ht_list)) * 100)))))
+    end = time.time()
+    print("Runtime: ", (end-start))
+
+
+main()
